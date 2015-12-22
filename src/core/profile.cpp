a05154882b95cd41438b65d050e3bb44a07226a9
Alexander Gorishnyak <kefir500@gmail.com>
2015-12-22 18:01:12 +0600

    Rename "Profile" gfx resources

--- src/core/profile.cpp
+++ src/core/profile.cpp
@@ -18,23 +18,23 @@ Profile::Profile(QString title, QIcon icon,
 
 void Profile::init()
 {
-    const QIcon ANDROID(":/gfx/profiles/android.png");
-    const QIcon BLACKBERRY(":/gfx/profiles/blackberry.png");
-    const QIcon KINDLE(":/gfx/profiles/kindle.png");
+    const QIcon ANDROID(":/gfx/devices/android.png");
+    const QIcon BLACKBERRY(":/gfx/devices/blackberry.png");
+    const QIcon AMAZON(":/gfx/devices/amazon.png");
 
     Profile android("Android Default", ANDROID, 36, 48, 72, 96, 144, 192);
     Profile bb_q10("BlackBerry Q10", BLACKBERRY, 90, 90, 90, 90, 90, 90); // Q10, Q5, Q10
     Profile bb_z10("BlackBerry Z10", BLACKBERRY, 110, 110, 110, 110, 110, 110);
     Profile bb_z30("BlackBerry Z30", BLACKBERRY, 96, 96, 96, 96, 96, 96); // Z30, Z3, Z30
     Profile bb_passport("BlackBerry Passport", BLACKBERRY, 144, 144, 144, 144, 144, 144);
-    Profile kindle1("Kindle Fire (1st Gen)", KINDLE, 36, 322, 72, 96, 144, 192);
-    Profile kindle2("Kindle Fire (2nd Gen)", KINDLE, 36, 365, 72, 96, 144, 192);
-    Profile kindle3("Kindle Fire HD 7\" (2nd Gen)", KINDLE, 36, 48, 425, 96, 144, 192);
-    Profile kindle4("Kindle Fire HD 8.9\" (2nd Gen)", KINDLE, 36, 48, 675, 96, 144, 192);
-    Profile kindle5("Kindle Fire HD 7\" (3rd Gen)", KINDLE, 36, 48, 375, 96, 144, 192);
-    Profile kindle6("Kindle Fire HDX 7\" (3rd Gen)", KINDLE, 36, 48, 72, 562, 144, 192);
-    Profile kindle7("Kindle Fire HDX 8.9\" (3rd Gen)", KINDLE, 36, 48, 72, 624, 144, 192);
-    Profile kindle8("Kindle Fire - All Models", KINDLE, 36, 365, 675, 624, 144, 192);
+    Profile kindle1("Kindle Fire (1st Gen)", AMAZON, 36, 322, 72, 96, 144, 192);
+    Profile kindle2("Kindle Fire (2nd Gen)", AMAZON, 36, 365, 72, 96, 144, 192);
+    Profile kindle3("Kindle Fire HD 7\" (2nd Gen)", AMAZON, 36, 48, 425, 96, 144, 192);
+    Profile kindle4("Kindle Fire HD 8.9\" (2nd Gen)", AMAZON, 36, 48, 675, 96, 144, 192);
+    Profile kindle5("Kindle Fire HD 7\" (3rd Gen)", AMAZON, 36, 48, 375, 96, 144, 192);
+    Profile kindle6("Kindle Fire HDX 7\" (3rd Gen)", AMAZON, 36, 48, 72, 562, 144, 192);
+    Profile kindle7("Kindle Fire HDX 8.9\" (3rd Gen)", AMAZON, 36, 48, 72, 624, 144, 192);
+    Profile kindle8("Kindle Fire - All Models", AMAZON, 36, 365, 675, 624, 144, 192);
     kindle1.presets[MDPI].info = "Carousel";
     kindle2.presets[MDPI].info = "Carousel";
     kindle3.presets[HDPI].info = "Carousel";
