6684b793326121cb872c7ba5d1ba6ed3fb65562c
Alexander Gorishnyak <kefir500@gmail.com>
2016-01-18 23:12:10 +0600

    Add application summary to docs

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,3 +1,17 @@
+///
+/// \mainpage APK Icon Editor
+///
+/// \section Summary
+/// - APK Icon Editor is a free, open-source and cross-platform tool designed to
+///   easily change APK resources such as icons, strings, images,
+///   application name and version, etc.
+///
+/// \section Details
+/// - Written in: C++/Qt
+/// - Source code: [GitHub](https://github.com/kefir500/apk-icon-editor)
+/// - License: [GNU GPL v3.0](http://www.gnu.org/licenses/gpl-3.0.html)
+///
+
 #include "application.h"
 
 int main(int argc, char *argv[])
