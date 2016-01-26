967d13d63d06ca21575f733d538ff09ffa4129b6
Alexander Gorishnyak <kefir500@gmail.com>
2016-01-26 13:59:52 +0600

    Make MainWindow constructor explicit

--- src/dialogs/mainwindow.h
+++ src/dialogs/mainwindow.h
@@ -33,7 +33,7 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 public slots:
