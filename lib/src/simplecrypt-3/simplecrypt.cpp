d3f3804d69ebba923baf827a13bdc993f4911e55
Alexander Gorishnyak <kefir500@gmail.com>
2016-02-16 17:31:23 +0600

    Fix Qt 5.5 missing QDataStream include

--- lib/src/simplecrypt-3/simplecrypt.cpp
+++ lib/src/simplecrypt-3/simplecrypt.cpp
@@ -29,6 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <QtDebug>
 #include <QtGlobal>
 #include <QDateTime>
+#include <QDataStream>
 #include <QCryptographicHash>
  
 SimpleCrypt::SimpleCrypt():
