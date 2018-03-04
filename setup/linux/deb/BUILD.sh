fceafd865f9cd05cfb47e3f6a6a1a7b2310eca4c
Alexander Gorishnyak <kefir500@gmail.com>
2018-03-04 13:53:38 +0600

    Add DEB build script

---
+++ setup/linux/deb/BUILD.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clean() {
+    rm ../apk-icon-editor_*.tar.xz ../apk-icon-editor_*.tar.gz ../apk-icon-editor_*.dsc ../apk-icon-editor_*.changes ../apk-icon-editor_*.build 2> /dev/null
+    rm -rf ./debian 2> /dev/null
+}
+
+cd ../../..
+
+clean
+rm ../apk-icon-editor_*.deb 2> /dev/null
+
+if dh_make -y -s -e kefir500@gmail.com -c gpl3 --createorig; then
+    rm -rf ./debian 2> /dev/null
+    cp -R ./setup/linux/deb/debian ./debian
+    debuild -uc -us
+    clean
+fi
