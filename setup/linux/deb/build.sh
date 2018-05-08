4ffdd7363c001c3e34cbb18f5a242b63b88befc1
Alexander Gorishnyak <kefir500@gmail.com>
2018-05-09 01:07:32 +0600

    Add maintainer info to DEB build script

--- setup/linux/deb/build.sh
+++ setup/linux/deb/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export DEBEMAIL="kefir500@gmail.com"
+export DEBFULLNAME="Alexander Gorishnyak"
+
 clean() {
     rm ../apk-icon-editor_*.tar.xz ../apk-icon-editor_*.tar.gz ../apk-icon-editor_*.dsc ../apk-icon-editor_*.changes ../apk-icon-editor_*.build 2> /dev/null
     rm -rf ./debian 2> /dev/null
@@ -10,7 +13,7 @@ cd ../../..
 clean
 rm ../apk-icon-editor_*.deb 2> /dev/null
 
-if dh_make -y -s -e kefir500@gmail.com -c gpl3 --createorig; then
+if dh_make -y -s -c gpl3 --createorig; then
     rm -rf ./debian 2> /dev/null
     cp -R ./setup/linux/deb/debian ./debian
     debuild -uc -us
